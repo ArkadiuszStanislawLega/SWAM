c4fa79f98cc7574e0958015b9bdd67ece94d0f33
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-06-24 00:06:40 +0200

    Add products list from database template in manage orders page

---
+++ SWAM/SWAM/Controls/Templates/ManageOrdersPage/ProductsListFromDbTemplate.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SWAM.Controls.Templates.ManageOrdersPage
+{
+    /// <summary>
+    /// Interaction logic for ProductsListFromDbTemplate.xaml
+    /// </summary>
+    public partial class ProductsListFromDbTemplate : UserControl
+    {
+        public ProductsListFromDbTemplate()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberRowIteration(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+    }
+}
