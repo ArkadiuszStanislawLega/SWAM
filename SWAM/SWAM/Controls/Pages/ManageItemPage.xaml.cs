2c1e72fd125eae567e3397dac66e2b5e1f14098b
Marcin Kupiński <Mlorism@outlook.com>
2019-06-25 22:00:45 +0200

    MagangeItemPage update funcjonalność przycisków

--- SWAM/SWAM/Controls/Pages/ManageItemPage.xaml.cs
+++ SWAM/SWAM/Controls/Pages/ManageItemPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SWAM.Controls.Windows;
 using SWAM.Enumerators;
 
 namespace SWAM
@@ -25,9 +26,10 @@ namespace SWAM
         public ManageItemPage() 
         {
             InitializeComponent();
-
             DataContext = this;
+            
         }
+
         #endregion
         public class Item
         {
@@ -48,19 +50,18 @@ namespace SWAM
                 Weight = weight;
                 Selling_price = selling_price;
             }
-
         }
 
         private List<Item> items = new List<Item>
         {
+            new Item(1, "Cegła", 10, 2, 0.2, 2.5),            
             new Item(2, "Taczka", 60, 100, 6, 60),
             new Item(3, "Piasek", 15, 80, 15, 10),
-            new Item(2, "Taczka", 60, 100, 6, 60),
             new Item(3, "Piasek", 15, 80, 15, 10),
-            new Item(2, "Taczka", 60, 100, 6, 60),
-            new Item(3, "Piasek", 15, 80, 15, 10),
-            new Item(2, "Taczka", 60, 100, 6, 60),
-            new Item(3, "Piasek", 15, 80, 15, 10),
+            new Item(4, "Deska", 20, 60, 0.5, 5),
+            new Item(5, "Kostka brukowa", 10, 10, 0.33, 2),
+            new Item(6, "Taczka", 70, 110, 6.5, 70),
+            new Item(7, "Wiadro", 30, 40, 1.5, 15),
         };
 
         public List<Item> Items
@@ -68,5 +69,45 @@ namespace SWAM
             get { return items; }
             set { items = value; }
         }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Windows.OfType<AddProductWindow>().Any()==true)
+            {
+                MessageBox.Show("Okno jest już otworzone.", "Okno otworzone", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                AddProductWindow _addWindow = new AddProductWindow();
+                _addWindow.Show();
+            }
+         }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Windows.OfType<EditProductWindow>().Any() == true)
+            {
+                MessageBox.Show("Okno jest już otworzone.", "Okno otworzone", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                EditProductWindow _editWindow = new EditProductWindow();
+                _editWindow.Show();
+            }                      
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Windows.OfType<DeleteProductWindow>().Any() == true)
+            {
+                MessageBox.Show("Okno jest już otworzone.", "Okno otworzone", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                DeleteProductWindow _deleteWindow = new DeleteProductWindow();
+                _deleteWindow.Show();
+            }           
+        }
+       
     }
 }
