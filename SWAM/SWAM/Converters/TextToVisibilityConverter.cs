965333325ec3905d10bfb30b41b9f57a91b3457b
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2019-07-21 18:28:17 +0200

    Update main window view: - replace title bar for menu bar - add new buttons mail and current user profile - add new big button style - mail

---
+++ SWAM/SWAM/Converters/TextToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SWAM.Converters
+{
+    /// <summary>
+    /// Converter to change text with number of messages to visibility in mail button.
+    /// When messages is 0 visibility are hidden.
+    /// </summary>
+    public class TextToVisibilityConverter : BaseValueConverter<TextToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueString = (string)value;
+
+            if (valueString == "") return Visibility.Hidden;
+            else return  Visibility.Visible;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
