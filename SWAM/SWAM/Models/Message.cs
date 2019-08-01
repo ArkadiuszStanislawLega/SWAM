a7650dbf0c1be65ed92312ba2454c82f852fdf8d
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2019-08-01 17:33:58 +0200

    Messsage Model - documentation.

--- SWAM/SWAM/Models/Message.cs
+++ SWAM/SWAM/Models/Message.cs
@@ -7,30 +7,87 @@ namespace SWAM.Models
 {
     public class Message
     {
+        /// <summary>
+        /// Current message Id in database.
+        /// </summary>
         private int _id;
-        public int SenderId { get; set; }
-        public int ReceiverId { get; set; }
+        /// <summary>
+        /// Title of current message.
+        /// </summary>
+        private string _titleOfMessage;
+        /// <summary>
+        /// Main content of current message.
+        /// </summary>
+        private string _contentOfMessage;
+        /// <summary>
+        /// Date of posting by sender current message.
+        /// </summary>
+        private DateTime _postDate;
+        /// <summary>
+        /// Date of reading by receiver current message.
+        /// </summary>
+        private DateTime? _dateOfReading;
+        /// <summary>
+        /// Flag indicating current message is readed.
+        /// </summary>
+        private bool _isReaded;
+        /// <summary>
+        /// Flag indicating current message is deleted by sender.
+        /// If true - sender does not see this message in his send e-mail box.
+        /// </summary>
+        private bool _isDeletedBySender;
+        /// <summary>
+        /// Flag indicating current message is deleted by receiver.
+        /// If true - receiver does not see this message in his receive e-mail box.
+        /// </summary>
+        private bool _isDeletedByReceiver;
+        /// <summary>
+        /// Current message Id in database.
+        /// </summary>
+        public int Id { get => _id; set => _id = value; }
 
+        /// <summary>
+        /// Main content of current message.
+        /// </summary>
+        public string ContentOfMessage { get => _contentOfMessage; set => _contentOfMessage = value; }
+        /// <summary>
+        /// Title of current message.
+        /// </summary>
+        public string TitleOfMessage { get => _titleOfMessage; set => _titleOfMessage = value; }
+        /// <summary>
+        /// Date of posting by sender current message.
+        /// </summary>
+        public DateTime PostDate { get => _postDate; set => _postDate = value; }
+        /// <summary>
+        /// Date of reading by receiver current message.
+        /// </summary>
+        public DateTime? DateOfReading { get => _dateOfReading; set => _dateOfReading = value; }
+        /// <summary>
+        /// Flag indicating current message is readed.
+        /// </summary>
+        public bool IsReaded { get => _isReaded; set => _isReaded = value; }
+        /// <summary>
+        /// Flag indicating current message is deleted by sender.
+        /// If true - sender does not see this message in his send e-mail box.
+        /// </summary>
+        public bool IsDeletedBySender { get => _isDeletedBySender; set => _isDeletedBySender = value; }
+        /// <summary>
+        /// Flag indicating current message is deleted by receiver.
+        /// If true - receiver does not see this message in his receive e-mail box.
+        /// </summary>
+        public bool IsDeletedByReceiver { get => _isDeletedByReceiver; set => _isDeletedByReceiver = value; }
+        /// <summary>
+        /// User - sender id in database.
+        /// </summary>
+        public int SenderId { get; set; }
+        /// <summary>
+        /// User - receiver id in database.
+        /// </summary>
+        public int ReceiverId { get; set; }
         private User _sender;
         private User _receiver;
-        private string _titleOfMessage;
-        private string _contentOfMessage;
-        private DateTime _postDate;
-        private DateTime? _dateOfReading;
-        private bool _isReaded;
-        private bool _isDeletedBySender;
-        private bool _isDeletedByReceiver;
-
-        public int Id { get => _id; set => _id = value; }
         public User Sender { get => _sender; set => _sender = value; }
         public User Receiver { get => _receiver; set => _receiver = value; }
-        public string ContentOfMessage { get => _contentOfMessage; set => _contentOfMessage = value; }
-        public string TitleOfMessage { get => _titleOfMessage; set => _titleOfMessage = value; }
-        public DateTime PostDate { get => _postDate; set => _postDate = value; }
-        public DateTime? DateOfReading { get => _dateOfReading; set => _dateOfReading = value; }
-        public bool IsReaded { get => _isReaded; set => _isReaded = value; }
-        public bool IsDeletedBySender { get => _isDeletedBySender; set => _isDeletedBySender = value; }
-        public bool IsDeletedByReceiver { get => _isDeletedByReceiver; set => _isDeletedByReceiver = value; }
 
         private static readonly ApplicationDbContext DB_CONTEXT = new ApplicationDbContext();
 
