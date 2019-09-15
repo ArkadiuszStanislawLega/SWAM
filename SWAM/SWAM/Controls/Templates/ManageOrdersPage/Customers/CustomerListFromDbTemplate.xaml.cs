42555da7ba8497c2ae4796b4d5977e5f25ae49a0
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-09-15 23:05:05 +0200

    Add autocomplete for customer search box

--- SWAM/SWAM/Controls/Templates/ManageOrdersPage/Customers/CustomerListFromDbTemplate.xaml.cs
+++ SWAM/SWAM/Controls/Templates/ManageOrdersPage/Customers/CustomerListFromDbTemplate.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SWAM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,46 @@ namespace SWAM.Controls.Templates.ManageOrdersPage.Customers
     /// </summary>
     public partial class CustomerListFromDbTemplate : UserControl
     {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+        private List<Customer> _customers = new List<Customer>();
+
         public CustomerListFromDbTemplate()
         {
             InitializeComponent();
+            _customers = GetCustomersFromDb();
+
+            lvUsers.ItemsSource = _customers;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvUsers.ItemsSource);
+            view.Filter = UserFilter;
+            
+        }
+
+        private List<Customer> GetCustomersFromDb() { return _context.Customers.ToList(); }
+
+        private void CreateCustomersData()
+        {
+            var customerList = GetCustomersFromDb();
+
+            foreach (var customer in customerList)
+            {
+                _customers.Add(customer);
+            }
+        }
+
+        private bool UserFilter(object item)
+        {
+            Customer customer = item as Customer;
+            if (customer != null)
+            {
+                string CombinedName = customer.Name + " " + customer.Surname;
+                return (CombinedName.IndexOf(txtFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+            }
+            return false;
+        }
+
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(lvUsers.ItemsSource).Refresh();
         }
     }
 }
