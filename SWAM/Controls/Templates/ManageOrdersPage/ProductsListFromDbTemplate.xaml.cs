d9447e78294f3eb31db29061c75d2d8f5219542a
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-09-27 21:43:42 +0200

    Add GetProductListFromUserWarehouse method in ProductsListFromDbTemplate page

--- SWAM/Controls/Templates/ManageOrdersPage/ProductsListFromDbTemplate.xaml.cs
+++ SWAM/Controls/Templates/ManageOrdersPage/ProductsListFromDbTemplate.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using SWAM.Models;
 using System.Collections;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -13,6 +14,17 @@ namespace SWAM.Controls.Templates.ManageOrdersPage
         public ProductsListFromDbTemplate()
         {
             InitializeComponent();
+        }
+
+        private List<Product> GetProductListFromUserWarehouse()
+        {
+            var accessUsersToWarehouses = AccessUsersToWarehouses.GetUserAccess(SWAM.MainWindow.LoggedInUser.Id);
+            if (accessUsersToWarehouses != null)
+            {
+                var product = new Product();
+                return product.GetProductsFromWarehouse(accessUsersToWarehouses.UserId);
+            }
+            return null;
         }
 
         private void NumberRowIteration(object sender, DataGridRowEventArgs e)
