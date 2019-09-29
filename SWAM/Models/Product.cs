a958c80b833fa0bdd9ede586c2fc94a68e96b940
Marcin Kupiński <Mlorism@gmail.com>
2019-09-29 19:09:37 +0200

    Dodanie metod umożliwiających dodawanie, edycję i usuwanie produktów.

--- SWAM/Models/Product.cs
+++ SWAM/Models/Product.cs
@@ -19,9 +19,9 @@ namespace SWAM.Models
         IList<State> _states;
         IList<WarehouseOrderPosition> _warehouseOrderPositions;
         IList<CustomerOrderPosition> _customerOrderPositions;
-        ApplicationDbContext _context = new ApplicationDbContext();
-
-        public int Id { get => _id; set => _id = value; }
+		private static readonly ApplicationDbContext DB_CONTEXT = new ApplicationDbContext();
+				
+		public int Id { get => _id; set => _id = value; }
         public string Name { get => _name; set => _name = value; }
         public long Weigth { get => _weigth; set => _weigth = value; }
         public long Length { get => _length; set => _length = value; }
@@ -31,8 +31,53 @@ namespace SWAM.Models
         public IList<State> States { get => _states; set => _states = value; }
         public IList<WarehouseOrderPosition> WarehouseOrderPositions { get => _warehouseOrderPositions; set => _warehouseOrderPositions = value; }
         public IList<CustomerOrderPosition> CustomerOrderPositions { get => _customerOrderPositions; set => _customerOrderPositions = value; }
+		private static ApplicationDbContext _context
+		{
+			//TODO: Make all exceptions
+			get
+			{
+				return DB_CONTEXT;
+			}
+		}
+		public Product() { }	
 
-        public List<Product> GetProducts()
+		public Product(string name, long weight, long length, long width, long height, decimal price)
+		{
+
+			Name = name;
+			Weigth = weight;
+			Length = length;
+			Width = width;
+			Height = height;
+			Price = price;
+		}
+		public static void AddNewProduct(Product product)
+		{
+			if (product != null)
+			{
+				_context.Products.Add(product);
+				_context.SaveChanges();
+			}
+		}
+		public static void EditProduct(Product product)
+		{
+			_context.Products.FirstOrDefault(p => p.Id == product.Id).Name = product.Name;
+			_context.Products.FirstOrDefault(p => p.Id == product.Id).Weigth = product.Weigth;
+			_context.Products.FirstOrDefault(p => p.Id == product.Id).Length = product.Length;
+			_context.Products.FirstOrDefault(p => p.Id == product.Id).Width = product.Width;
+			_context.Products.FirstOrDefault(p => p.Id == product.Id).Height = product.Height;
+			_context.Products.FirstOrDefault(p => p.Id == product.Id).Price = product.Price;
+			_context.SaveChanges();
+		}
+
+		public static void DeleteProduct(Product product)
+		{
+			var dbproduct =_context.Products.FirstOrDefault(p => p.Id == product.Id);
+			_context.Products.Remove(dbproduct);
+			_context.SaveChanges();
+		}
+
+		public List<Product> GetProducts()
         {
             return _context.Products.ToList();
         }
