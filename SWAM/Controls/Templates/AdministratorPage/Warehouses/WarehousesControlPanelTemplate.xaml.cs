0071e3c2a30539b3efe2b8469ea383de7bb74563
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2019-10-01 19:20:45 +0200

    Issue show warehouse profile - fixed.

--- SWAM/Controls/Templates/AdministratorPage/Warehouses/WarehousesControlPanelTemplate.xaml.cs
+++ SWAM/Controls/Templates/AdministratorPage/Warehouses/WarehousesControlPanelTemplate.xaml.cs
@@ -61,11 +61,11 @@ namespace SWAM.Templates.AdministratorPage
         /// <return>Chosen warehouse profile.</return>
         private WarehouseProfileTemplate CreateWarehouseProfile(int warehouseIndexInWaregohouseList)
         {
-            throw new NotImplementedException();
-            //var context = new ApplicationDbContext();
-            //return new WarehouseProfileTemplate() {
-            //    DataContext = context.Warehouses.Include(a => a.Address).FirstOrDefault(w => w.Id == warehouseIndexInWaregohouseList)
-            //};
+            var context = new ApplicationDbContext();
+            return new WarehouseProfileTemplate()
+            {
+                DataContext = context.Warehouses.FirstOrDefault(w => w.Id == warehouseIndexInWaregohouseList)
+            };
         }
         #endregion
 
