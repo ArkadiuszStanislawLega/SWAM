3fcff05cb56150af22b6ac978b5630bb006f748e
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-10-15 18:51:24 +0200

    Add ProductOrderListViewModel

---
+++ SWAM/Models/ProductOrderList/ProductOrderListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+
+namespace SWAM.Models.ProductOrderList
+{
+    public class ProductOrderListViewModel
+    {
+        private readonly ObservableCollection<Product> _products = new ObservableCollection<Product>();
+        public ObservableCollection<Product> Products => this._products;
+
+        #region Singletone Pattern
+
+        private static readonly ProductOrderListViewModel _instance = new ProductOrderListViewModel();
+        public static ProductOrderListViewModel Instance => _instance;
+        #endregion 
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Clear()
+        {
+            if (_products.Count > 0)
+                _products.Clear();
+        }
+    }
+}
