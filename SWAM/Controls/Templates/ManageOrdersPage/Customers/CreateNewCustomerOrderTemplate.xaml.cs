3bb8bc43e0f309e7266eb57941995a82035db227
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-10-17 21:40:05 +0200

    Add order personal collection checkbox

--- SWAM/Controls/Templates/ManageOrdersPage/Customers/CreateNewCustomerOrderTemplate.xaml.cs
+++ SWAM/Controls/Templates/ManageOrdersPage/Customers/CreateNewCustomerOrderTemplate.xaml.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SWAM.Controls.Templates.ManageOrdersPage.Customers
 {
