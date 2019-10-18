b1178314d3b5b00ae0a7b0f42f885b08d6db4cfd
Arkadiusz Stanislaw Lega <arkadiusz.stanislaw.lega@gmail.com>
2019-10-18 03:45:04 +0200

    Bool to string converter - payment status.

---
+++ SWAM/Converters/BoolToPLstringConerter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace SWAM.Converters
+{
+    public class BoolToPLstringConerter : BaseValueConverter<BoolToPLstringConerter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool status)
+            {
+                if (status) return "opłacono";
+                else return "nie opłacono";
+            }
+            else return value.ToString();
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
