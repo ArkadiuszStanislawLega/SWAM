f2d492b9b19fa1e4ef850b7bdfc429930af47be6
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-10-27 20:55:23 +0100

    Add converter providing initial value for product quantity

---
+++ SWAM/Converters/InitialValueForProductQuantity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SWAM.Converters
+{
+    public class InitialValueForProductQuantity :BaseValueConverter<InitialValueForProductQuantity>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value = 1;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
