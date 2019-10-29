6ac53fa39eabd3b39478b80e170949882241b3c9
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-10-29 11:04:28 +0100

    Add payment order page

---
+++ SWAM/Controls/Templates/ManageOrdersPage/PaymentOrderTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SWAM.Controls.Templates.ManageOrdersPage
+{
+    /// <summary>
+    /// Interaction logic for PaymentOrderTemplate.xaml
+    /// </summary>
+    public partial class PaymentOrderTemplate : UserControl
+    {
+        public PaymentOrderTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
