9266c9803f573e4a07adbd53f0785e28f9cf244d
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-11-02 12:11:33 +0100

    Move customer address template to customer folder

---
+++ SWAM/Controls/Templates/ManageOrdersPage/NewOrder/Customers/CustomerAddressFormTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SWAM.Controls.Templates.ManageOrdersPage.NewOrder
+{
+    /// <summary>
+    /// Interaction logic for AddressOrderTemplate.xaml
+    /// </summary>
+    public partial class CustomerAddressFormTemplate : UserControl
+    {
+        public CustomerAddressFormTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
