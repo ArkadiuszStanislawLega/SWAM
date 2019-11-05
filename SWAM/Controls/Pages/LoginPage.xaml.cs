ea976404cc6460d8e4aaa0efac017bb8a37b69e8
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2019-11-05 10:15:39 +0100

    Updated login page - added counter of failed login attempts and disable possibility to login to the system after 3 attempts.

--- SWAM/Controls/Pages/LoginPage.xaml.cs
+++ SWAM/Controls/Pages/LoginPage.xaml.cs
@@ -13,6 +13,11 @@ namespace SWAM.Controls.Pages
     /// </summary>
     public partial class LoginPage : BasicPage
     {
+        /// <summary>
+        /// The number of failed attempts to log into the system.
+        /// </summary>
+        private int _failedLogingAttempts = 0;
+
         public LoginPage()       
         {
             InitializeComponent();
@@ -29,9 +34,17 @@ namespace SWAM.Controls.Pages
         {
             //Try to login in.
             if (User.TryLogIn(UserLogin.Text, UserPassword.Password) && MainWindow.CurrentInstance != null)
-                    MainWindow.CurrentInstance.ChangeContent(PagesUserControls.MessagesPage);
+            {
+                MainWindow.CurrentInstance.ChangeContent(PagesUserControls.MessagesPage);
+                this._failedLogingAttempts = 0;
+            }
+            else
+            {
+                this._failedLogingAttempts++;
+                if (this._failedLogingAttempts >= 3)
+                    this.LoginButton.IsEnabled = false;
+            }
             
-
             this.UserPassword.Password = "";
         }
         #endregion
