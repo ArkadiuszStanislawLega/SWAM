6f8c34def6b5bdac2a1e0791a4c00cd24baf91f0
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-11-09 14:14:55 +0100

    Drop relationship between ExternalSupplierEmailAddresses and EmailAdresses tables

--- SWAM/Models/ExternalSupplier/ExternalSupplierEmailAddress.cs
+++ SWAM/Models/ExternalSupplier/ExternalSupplierEmailAddress.cs
@@ -7,9 +7,20 @@ using System.Threading.Tasks;
 
 namespace SWAM.Models.ExternalSupplier
 {
-    [Table("ExternalSupplierEmailAddresses")]
-    public class ExternalSupplierEmailAddress : EmailAddress
+    public class ExternalSupplierEmailAddress
     {
+        /// <summary>
+        /// Email address number Id in database.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Person's email address.
+        /// </summary>
+        public string AddressEmail { get; set; }
+        /// <summary>
+        /// Note to email address.
+        /// </summary>
+        public string Note { get; set; }
         public ExternalSupplier ExternalSupplier { get; set; }
         public int ExternalSupplierId { get; set; }
     }
