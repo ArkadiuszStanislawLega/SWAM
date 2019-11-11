fbfd2573b1c46d897940aeea6133c012a96a08a5
Marcin Kupiński <Mlorism@gmail.com>
2019-11-11 20:09:49 +0100

    Display data with selected warehouse order.

--- SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/Manage/WarehouseOrderProfileTemplate.xaml.cs
+++ SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/Manage/WarehouseOrderProfileTemplate.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SWAM.Enumerators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,10 @@ namespace SWAM.Controls.Templates.ManageOrdersPage.ManageWarehouseOrdersPage.Man
         public WarehouseOrderProfileTemplate()
         {
             InitializeComponent();
-        }
-    }
+
+		}
+
+
+
+	}
 }
