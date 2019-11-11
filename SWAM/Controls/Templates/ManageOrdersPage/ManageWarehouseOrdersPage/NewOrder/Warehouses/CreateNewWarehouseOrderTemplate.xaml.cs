284fd516d6fbd06ef60799a51d3b1e57cb95b17a
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-11-11 19:27:50 +0100

    Add pages transition in warehouse orders

--- SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/NewOrder/Warehouses/CreateNewWarehouseOrderTemplate.xaml.cs
+++ SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/NewOrder/Warehouses/CreateNewWarehouseOrderTemplate.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SWAM.Controls.Templates.AdministratorPage;
+using SWAM.Enumerators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,17 +22,98 @@ namespace SWAM.Controls.Templates.ManageOrdersPage.ManageWarehouseOrdersPage.New
     /// </summary>
     public partial class CreateNewWarehouseOrderTemplate : BasicUserControl
     {
+        #region Properties
+        // Current visible page
+        WarehouseOrderVisiblePage visiblePage = WarehouseOrderVisiblePage.WarehousePage;
+        // Pages with its grid containers elements
+        Dictionary<WarehouseOrderVisiblePage, ScrollViewer> pages;
+        #endregion
+
+        #region Constructor
         public CreateNewWarehouseOrderTemplate()
         {
             InitializeComponent();
-        }
 
+            // Seed dictionary
+            pages = new Dictionary<WarehouseOrderVisiblePage, ScrollViewer>()
+            {
+                { WarehouseOrderVisiblePage.WarehousePage, warehouseElementsContainer},
+                { WarehouseOrderVisiblePage.ProductPage, productElementsContainer},
+                { WarehouseOrderVisiblePage.ExternalSupplierPage, externalSupplierElementsContainer}
+            };
+        }
+        #endregion
+
+        #region SwitchContentPreviousButton_Click
+        /// <summary>
+        /// Switches to previous tab 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SwitchContentPreviousButton_Click(object sender, RoutedEventArgs e)
         {
+            // Check if current page is first
+            if (visiblePage == WarehouseOrderVisiblePage.WarehousePage)
+                return;
 
+            // Check if current page is last 
+            if (pages.Last().Key == visiblePage)
+                switchContentNextButton.Content = "Następny";
+
+            // Decrease visibile page element
+            visiblePage--;
+
+            // Switch pages visibility
+            SwitchPagesVisibility();
         }
+        #endregion
 
+        #region SwitchContentNextButton_Click
+        /// <summary>
+        /// Switches to next tab
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SwitchContentNextButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Check if current page is last
+            if (pages.Last().Key == visiblePage)
+            {
+                // Submit form
+                CreateWarehouseOrder();
+                return;
+            }
+
+            // Check if current page is second last 
+            if (pages.Count - 2 == (int)visiblePage)
+                switchContentNextButton.Content = "Zatwierdź";
+
+            // Increase visibile page element
+            visiblePage++;
+
+            // Switch pages visibility
+            SwitchPagesVisibility();
+        }
+        #endregion
+
+        #region SwitchPagesVisibility
+        private void SwitchPagesVisibility()
+        {
+            foreach (var page in pages)
+            {
+                if (page.Key != visiblePage)
+                {
+                    page.Value.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    page.Value.Visibility = Visibility.Visible;
+                }
+            }
+        }
+        #endregion
+
+        private void CreateWarehouseOrder()
         {
 
         }
