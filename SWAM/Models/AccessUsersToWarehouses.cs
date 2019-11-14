877ea44b5b42010658efac355137c478c79029eb
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2019-11-14 07:53:59 +0100

    Update model  customer residental address - handle database exceptions.

--- SWAM/Models/AccessUsersToWarehouses.cs
+++ SWAM/Models/AccessUsersToWarehouses.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
-using System.Windows;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Validation;
 using SWAM.Strings;
