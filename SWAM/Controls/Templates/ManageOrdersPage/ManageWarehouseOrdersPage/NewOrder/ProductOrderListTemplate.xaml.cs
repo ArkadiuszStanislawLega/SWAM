a8e286f168bbe92bf23d05e5e394edf525c0c4b6
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-12-02 17:04:29 +0100

    Protect from selection of input values and zero insertion in warehouse's product list quantity field

--- SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/NewOrder/ProductOrderListTemplate.xaml.cs
+++ SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/NewOrder/ProductOrderListTemplate.xaml.cs
@@ -64,6 +64,29 @@ namespace SWAM.Controls.Templates.ManageOrdersPage.ManageWarehouseOrdersPage.New
         }
         #endregion
 
+        #region Quantity_LostFocus
+        /// <summary>
+        /// Checks if quantity is not 0 after lost focus of the field
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Quantity_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var quantity = sender as TextBox;
+            var warehouseOrderPosition = quantity.DataContext as WarehouseOrderPosition;
+
+            if (quantity.Text == String.Empty)
+                return;
+
+            if (quantity.Text == "0")
+            {
+                quantity.Text = "1";
+                warehouseOrderPosition.Quantity = 1;
+                return;
+            }
+        }
+        #endregion
+
         #region NumberValidationTextBox
         /// <summary>
         /// Make quantity textbox field accept numeric input type only
