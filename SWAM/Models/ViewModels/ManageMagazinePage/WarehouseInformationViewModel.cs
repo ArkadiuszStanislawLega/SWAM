17c8fea54fb8a7a46d8259fc4cea77cb8607902b
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-12-10 20:57:30 +0100

    Add view model

---
+++ SWAM/Models/ViewModels/ManageMagazinePage/WarehouseInformationViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAM.Models.ViewModels.ManageMagazinePage
+{
+    public class WarehouseInformationViewModel : INotifyPropertyChanged
+    {
+        private Warehouse.Warehouse _warehouse;
+        public Warehouse.Warehouse Warehouse { get { return _warehouse; } private set { _warehouse = value; NotifyPropertyChanged("Warehouse"); } }
+
+        #region Singletone Pattern
+        private static readonly WarehouseInformationViewModel _instance = new WarehouseInformationViewModel();
+        public static WarehouseInformationViewModel Instance => _instance;
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region SetWarehouse
+        public void SetWarehouse(Warehouse.Warehouse warehouse)
+        {
+            Warehouse = warehouse;
+        }
+        #endregion
+
+        #region Clear
+        public void Clear()
+        {
+            Warehouse = null;
+        }
+        #endregion
+    }
+}
