3af67250998fa310c8d206384e13292233695b5d
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-12-14 19:29:09 +0100

    Implement filtering datagrid method

--- SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/NewOrder/ProductsListFromDbTemplate.xaml.cs
+++ SWAM/Controls/Templates/ManageOrdersPage/ManageWarehouseOrdersPage/NewOrder/ProductsListFromDbTemplate.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using SWAM.Models;
 using SWAM.Models.ViewModels.CreateNewWarehouseOrder;
 using SWAM.Models.Warehouse;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace SWAM.Controls.Templates.ManageOrdersPage.ManageWarehouseOrdersPage.NewOrder
 {
@@ -43,7 +45,20 @@ namespace SWAM.Controls.Templates.ManageOrdersPage.ManageWarehouseOrdersPage.New
 
         private void WarehouseDatagridFilter_TextChanged(object sender, TextChangedEventArgs e)
         {
+            TextBox filterTextBox = sender as TextBox;
+            string searchingText = filterTextBox.Text;
+            ICollectionView collectionView = CollectionViewSource.GetDefaultView(Products.ItemsSource);
 
+            if (searchingText == string.Empty)
+                collectionView.Filter = null;
+            else
+            {
+                collectionView.Filter = @object =>
+                {
+                    Product product = @object as Product;
+                    return (product.Name.ToUpper().StartsWith(searchingText.ToUpper()));
+                };
+            }
         }
     }
 }
