4be601781bfcc0d778e836f693dc2602f8a375e3
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-01-09 20:19:11 +0100

    Update about page - added dependance of back button visibility.

--- SWAM/Controls/Pages/AboutPage.xaml.cs
+++ SWAM/Controls/Pages/AboutPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SWAM.Controls.Pages
+﻿using System.Windows;
+
+namespace SWAM.Controls.Pages
 {
     /// <summary>
     /// Logika interakcji dla klasy AboutPage.xaml
@@ -10,9 +12,20 @@
             InitializeComponent();
         }
 
-        private void Back_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            SWAM.MainWindow.Instance.ChangeContent(Enumerators.PagesUserControls.LoginPage);
-        }
+        /// <summary>
+        /// Action after click back button.
+        /// Change main content of SWAM to login page.
+        /// </summary>
+        /// <param name="sender">Back button.</param>
+        /// <param name="e">Event click.</param>
+        private void Back_Click(object sender, System.Windows.RoutedEventArgs e) => MainWindow.Instance.ChangeContent(Enumerators.PagesUserControls.LoginPage);
+        
+        /// <summary>
+        /// Action after change visibilty of current user control.
+        /// </summary>
+        /// <param name="sender">About page.</param>
+        /// <param name="e">Event changing page visibility.</param>
+        private void BasicPage_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e) => this.BackButton.Visibility = MainWindow.LoggedInUser == null ? Visibility.Visible : Visibility.Hidden;
+        
     }
 }
